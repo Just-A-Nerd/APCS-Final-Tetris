e4746fc1aef8781f0c709056b63cbbe57c7ebc86
chickenchuck <ahollaba@robostorm.org>
2017-05-07 18:56:36 -0400

    added L block

--- src/PlayingField.java
+++ src/PlayingField.java
@@ -2,5 +2,20 @@ public class PlayingField
 {
 	private int height;
 	private int width;
-	public BlockSquare[][] field = new BlockSquare[height][width];
+	private BlockSquare[][] field = new BlockSquare[height][width];
+	
+	public BlockSquare[][] getField()
+	{
+		return field;
+	}
+	
+	public void makeBlockAppear()
+	{
+		
+	}
+	
+	public void clearLines()
+	{
+		
+	}
 }
