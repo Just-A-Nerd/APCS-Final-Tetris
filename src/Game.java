7b27612ac0c6bb7f9bac6c7a2c31a9c1e732bd51
Justin Neria <justin6399@gmail.com>
2017-05-10 08:06:34 -0400

    Draw flaying field  and create update
    update method to be filled later

--- src/Game.java
+++ src/Game.java
@@ -4,4 +4,8 @@
 	{
 		new Window();
 	}
+	public void update()
+	{
+		
+	}
 }
--- src/GraphicsDrawer.java
+++ src/GraphicsDrawer.java
@@ -10,6 +10,12 @@ public class GraphicsDrawer
 		int blockSize = 40;
 		int borderSize = 5;
 	    Graphics2D g2 = (Graphics2D) g;
+	    g2.setPaint(new Color(0, 0 ,0 ));
+	    g2.fill(new Rectangle2D.Double(50, 50,PlayingField.getWidth() * blockSize, PlayingField.getHeight() * blockSize));
+	    g2.setPaint(new Color(255, 255, 255));
+	    g2.drawLine(50, 50, 50 , 50 + PlayingField.getHeight()* (blockSize - 2));
+	    g2.drawLine(50, 50 + PlayingField.getHeight() * (blockSize - 2), 50 + PlayingField.getWidth() * blockSize , 50 + PlayingField.getHeight() * (blockSize - 2));
+	    g2.drawLine(50 + PlayingField.getWidth() * blockSize, 50, 50 + PlayingField.getWidth() * blockSize,  50 + PlayingField.getHeight() * (blockSize - 2));
 	    BlockSquare redBlock = new BlockSquare(new Color(255,0,0), new Color(200,0,0));
 	    g2.setPaint(redBlock.getOutlineColor());
 	    g2.fill(new Rectangle2D.Double(50,50,blockSize,blockSize));
@@ -19,9 +25,7 @@ public class GraphicsDrawer
 	    g2.draw(new Rectangle2D.Double(850, 200, 100, 100));//Next Block Display
 	    g2.draw(new Rectangle2D.Double(650, 50, 300, 100));//Score Display
 	    g2.drawString("Score", 775, 100);
-	    g2.drawLine(50, 50, 50 ,PlayingField.getHeight()*50);
-	    g2.drawLine(50, PlayingField.getHeight()*50, PlayingField.getWidth() * 50, PlayingField.getHeight()*50);
-	    g2.drawLine(600, 50, 600, 900);
+	    
 	    
 	    
 	    
